d2764035dc85ee6a932737bc1a042c5315a35962
Bleriot Noguia <nstevebleriot@yahoo.fr>
2021-04-07 19:47:50 +0100

    img bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import logo from "/img/logo-brand.svg";
+import beach_work from "/img/beach-work.jpg";
 
 import popularDestinations from "./data/popularDestinations";
 import DestinationCard from "./components/DestinationCard";
@@ -9,10 +11,10 @@ export default function App() {
       <div className="bg-gray-100 grid lg:grid-cols-2 2xl:grid-cols-5">
         <div className="px-8 py-12 max-w-md mx-auto sm:max-w-xl lg:px-12 lg:py-24 lg:max-w-full xl:mr-0 2xl:col-span-2">
           <div className="xl:max-w-xl">
-            <img className="h-10" src="/img/logo-brand.svg" alt="Workcation" />
+            <img className="h-10" src={logo} alt="Workcation" />
             <img
               className="mt-6 rounded-lg shadow-xl sm:mt-8 sm:h-64 sm:w-full sm:object-cover object-center lg:hidden"
-              src="/img/beach-work.jpg"
+              src={beach_work}
               alt="Woman workcationing on the beach"
             />
             <h1 className="mt-6 text-2xl font-headline tracking-tight font-semibold text-gray-900 sm:mt-8 sm:text-4xl lg:text-3xl xl:text-4xl">
--- src/data/popularDestinations.js
+++ src/data/popularDestinations.js
@@ -1,44 +1,51 @@
+import img1 from "/img/toronto.jpg"
+import img2 from "/img/malibu.jpg"
+import img3 from "/img/chicago.jpg"
+import img4 from "/img/seattle.jpg"
+import img5 from "/img/colorado.jpg"
+import img6 from "/img/miami.jpg"
+
 export default [
     {
       city: "Toronto",
       averagePrice: 120,
       propertyCount: 76,
-      imageUrl: "/img/toronto.jpg",
+      imageUrl: img1,
       imageAlt: "Toronto skyline",
     },
     {
       city: "Malibu",
       averagePrice: 215,
       propertyCount: 43,
-      imageUrl: "/img/malibu.jpg",
+      imageUrl: img2,
       imageAlt: "Cliff in Malibu",
     },
     {
       city: "Chicago",
       averagePrice: 130,
       propertyCount: 115,
-      imageUrl: "/img/chicago.jpg",
+      imageUrl: img3,
       imageAlt: "Chicago skyline",
     },
     {
       city: "Seattle",
       averagePrice: 135,
       propertyCount: 63,
-      imageUrl: "/img/seattle.jpg",
+      imageUrl: img4,
       imageAlt: "Seattle skyline",
     },
     {
       city: "Colorado",
       averagePrice: 85,
       propertyCount: 47,
-      imageUrl: "/img/colorado.jpg",
+      imageUrl: img5,
       imageAlt: "Lake in Colorado",
     },
     {
       city: "Miami",
       averagePrice: 115,
       propertyCount: 86,
-      imageUrl: "/img/miami.jpg",
+      imageUrl: img6,
       imageAlt: "Beach in Miami",
     },
   ];
